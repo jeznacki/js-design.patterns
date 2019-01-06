d76148334d61ba15a7dc4f4666985d7772e09562
jeznacki <jeznacki13@gmail.com>
2019-01-06 14:03:27 +0100

    decorator & facade patterns added created

--- structural/decorator.js
+++ structural/decorator.js
@@ -1,7 +1,76 @@
 /*
 
-Bridge pattern - allows two components, a client and a service, to work together
- with each component having its own interface.
-Bridge is a high-level architectural pattern and its main goal is to write better code through two levels of abstraction.
+Decorator pattern - The decorator pattern (also known as Wrapper, an alternative naming shared with the Adapter pattern) is a design pattern that allows the behavior to be added to an individual object, e
+either statically or dynamically, without affecting the behavior of other objects from the same class.
+
 
 */
+
+//=======Single decorator
+
+function vehicle( vehicleType ){
+
+    this.vehicleType = vehicleType || "car";
+    this.model = "default";
+    this.license = "00000-000";
+
+}
+
+var truck = new vehicle( "truck" );
+
+truck.setModel = function( modelName ){
+    this.model = modelName;
+};
+
+truck.setColor = function( color ){
+    this.color = color;
+};
+
+truck.setModel( "CAT" );
+truck.setColor( "blue" );
+
+console.log( truck ); // vehicle:truck, model:CAT, color: blue
+
+// "vehicle" is still unaltered
+var secondInstance = new vehicle( "car" );
+console.log( secondInstance );
+
+
+
+
+//=======Multiple decorators
+
+
+function MacBook() {
+    this.cost = function () { return 997; };
+    this.screenSize = function () { return 13.3; };
+}
+
+/*Decorator 1*/
+function Memory(macbook) {
+    var v = macbook.cost();
+    macbook.cost = function() {
+        return v + 75;
+    }
+}
+/*Decorator 2*/
+function Engraving( macbook ){
+    var v = macbook.cost();
+    macbook.cost = function(){
+        return  v + 200;
+    };
+}
+
+/*Decorator 3*/
+function Insurance( macbook ){
+    var v = macbook.cost();
+    macbook.cost = function(){
+        return  v + 250;
+    };
+}
+var mb = new MacBook();
+Memory(mb);
+Engraving(mb);
+Insurance(mb);
+console.log(mb.cost()); //1522
+console.log(mb.screenSize()); //13.3
--- structural/facade.js
+++ structural/facade.js
@@ -1,7 +1,67 @@
 /*
 
-Bridge pattern - allows two components, a client and a service, to work together
- with each component having its own interface.
-Bridge is a high-level architectural pattern and its main goal is to write better code through two levels of abstraction.
+Facade pattern - provides an interface that simplifies complex functionalities
 
 */
+
+
+
+/* Es 6 */
+
+class ShopFacade {
+    constructor() {
+        this.discount = new Discount();
+        this.shipping = new Shipping();
+        this.fees = new Fees();
+    }
+
+    calc(price) {
+        price = this.discount.calc(price);
+        price = this.fees.calc(price);
+        price += this.shipping.calc();
+        return price;
+    }
+}
+
+class Discount {
+    calc(value) {
+        return value * 0.9;
+    }
+}
+
+class Shipping {
+    calc() {
+        return 5;
+    }
+}
+
+class Fees {
+    calc(value) {
+        return value * 1.05;
+    }
+}
+
+
+/*ES 5 */
+
+var shopFacade = {
+    calc: function(price) {
+        price = discount(price);
+        price = fees(price);
+        price += shipping();
+        return price;
+    }
+};
+
+function discount(value) {
+    return value * 0.9;
+}
+
+function shipping() {
+    return 5;
+}
+
+function fees(value) {
+    return value * 1.05;
+}
+
