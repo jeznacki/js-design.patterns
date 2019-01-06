fbfbd8415708ef5a16d91e3ebc663e8e38bfa586
jeznacki <jeznacki13@gmail.com>
2019-01-06 13:23:05 +0100

    Bulider pattern added

--- creational/bulider.js
+++ creational/bulider.js
@@ -1,7 +1,100 @@
 /*
 
-Singleton is a  creational design pattern where only one instance of a class can exist.
-If no instance of the singleton class exists then a new instance is created and returned
-but if an instance already exists then the reference to the existing instance is returned.
+Bulider pattern - are used to separate the complexities of the creation logic from the final representation.
 
 */
+
+/* ES 6 */
+
+class ProductBuilder {
+    constructor() {
+        this.name = 'A Product'
+        this.price = 9.99
+        this.category = 'other'
+    }
+
+    withName(name) {
+        this.name = name
+        return this
+    }
+
+    withPrice(price) {
+        this.price = price
+        return this
+    }
+
+    withCategory(category) {
+        this.category = category
+        return this
+    }
+
+    build() {
+        return {
+            name: this.name,
+            price: this.price,
+            category: this.category,
+        }
+    }
+}
+
+console.log(
+    new ProductBuilder()
+        .withName('Harry Potter')
+        .withCategory('book')
+        .build()
+)
+
+// =>
+//    {
+//      name: 'Harry Potter',
+//      price: 9.99,
+//      category: 'book'
+//    }
+
+
+/* ES 5 */
+
+function ProductBuilder() {
+
+    this.name = 'A Product';
+    this.price = 9.99;
+    this.category = 'other';
+
+    this.withName = function(name){
+        this.name = name;
+        return this
+    }
+
+    this.withPrice = function(price) {
+        this.price = price;
+        return this
+    }
+
+    this.withCategory = function(category){
+        this.category = category;
+        return this
+    }
+
+    this.build = function() {
+        return {
+            name: this.name,
+            price: this.price,
+            category: this.category,
+        }
+    };
+
+}
+
+
+console.log(
+    new ProductBuilder()
+        .withName('Harry Potter')
+        .withCategory('book')
+        .build()
+)
+// =>
+//    {
+//      name: 'Harry Potter',
+//      price: 9.99,
+//      category: 'book'
+//    }
