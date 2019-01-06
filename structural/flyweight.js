99f2fcfc059ef341d6fccedaf36a912de5ce4420
jeznacki <jeznacki13@gmail.com>
2019-01-06 14:08:19 +0100

    flyweight pattern added

--- structural/flyweight.js
+++ structural/flyweight.js
@@ -1,7 +1,43 @@
 /*
 
-Bridge pattern - allows two components, a client and a service, to work together
- with each component having its own interface.
-Bridge is a high-level architectural pattern and its main goal is to write better code through two levels of abstraction.
+Flyweight pattern - The Flyweight pattern is a classical structural solution for optimizing code
+that is repetitive, slow, and inefficiently shares data. It aims to minimize the use of memory
+ in an application.  - see memoization & cacheing
 
 */
+
+/* Es6 */
+class Color {
+    constructor(name){
+        this.name = name
+    }
+}
+
+class colorFactory {
+    constructor(name){
+        this.colors = {};
+    }
+    create(name) {
+        let color = this.colors[name];
+        if(color) return color;
+        this.colors[name] = new Color(name);
+        return this.colors[name];
+    }
+};
+
+/* ES5 */
+
+function Color(name) {
+    this.name = name;
+}
+
+var colorFactory = {
+    colors: {},
+    create: function(name) {
+        var color = this.colors[name];
+        if(color) return color;
+
+        this.colors[name] = new Color(name);
+        return this.colors[name];
+    }
+};
