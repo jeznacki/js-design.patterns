a934d91660558ec6511bfa6de539e2c95cb5d14e
Jacek <jeznacki13@gmail.com>
2019-01-06 11:56:25 +0100

    Update factory.js

---
+++ creational/factory.js
@@ -0,0 +1,56 @@
+/*
+==== Creational Patterns =====
+
+## Factory Pattern
+
+Factory Pattern is class-based creational pattern.
+It provides a generic interface that delegates the responsibility of object instantiation to its subclasses.
+
+This pattern is used when we need to manage or manipulate collections of objects that are different yet have many similar characteristics.
+
+*/
+
+class BallFactory {
+  constructor() {
+    this.createBall = function(type) {
+      let ball;
+      if (type === 'football' || type === 'soccer') ball = new Football();
+      else if (type === 'basketball') ball = new Basketball();
+      ball.roll = function() {
+        return `The ${this._type} is rolling.`;
+      };
+
+      return ball;
+    };
+  }
+}
+
+class Football {
+  constructor() {
+    this._type = 'football';
+    this.kick = function() {
+      return 'You kicked the football.';
+    };
+  }
+}
+
+class Basketball {
+  constructor() {
+    this._type = 'basketball';
+    this.bounce = function() {
+      return 'You bounced the basketball.';
+    };
+  }
+}
+
+// creating objects
+const factory = new BallFactory();
+
+const myFootball = factory.createBall('football');
+const myBasketball = factory.createBall('basketball');
+
+console.log(myFootball.roll()); // The football is rolling.
+console.log(myBasketball.roll()); // The basketball is rolling.
+console.log(myFootball.kick()); // You kicked the football.
+console.log(myBasketball.bounce()); // You bounced the basketball.
+
